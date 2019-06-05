68165aa49cdb1ac23b2efb2c32180ab0cceb4f52
Dave <dave.e.siegel@gmail.com>
2019-06-04 21:30:49 -0400

    default mood created with value of 0.5

--- db/models/User.js
+++ db/models/User.js
@@ -132,6 +132,10 @@ const User = db.define(
           return user;
         });
       },
+      afterCreate: function(user) {
+        console.log('USER ID', user.id);
+        return db.model('mood').create({ userId: user.id, value: 0.5 });
+      },
     },
   }
 );
