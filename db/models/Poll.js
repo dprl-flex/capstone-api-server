045eebb4b4ec401c2686e7f35beba2e386563e32
Lauren West <laurenwest12@gmail.com>
2019-06-07 15:34:53 -0400

    added enum to polls

--- db/models/Poll.js
+++ db/models/Poll.js
@@ -1,30 +1,38 @@
 const db = require('../db');
 const { Sequelize } = db;
 
-const Poll = db.define('poll', {
+const Poll = db.define(
+  'poll',
+  {
     id: {
-        type: Sequelize.UUID,
-        primaryKey: true,
-        defaultValue: Sequelize.UUIDV4
+      type: Sequelize.UUID,
+      primaryKey: true,
+      defaultValue: Sequelize.UUIDV4
     },
     text: {
-        type: Sequelize.TEXT,
-        allowNull: false,
-        validate: {
-            notEmpty: {
-                args: true,
-                msg: 'You must enter a question'
-            }
+      type: Sequelize.TEXT,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          args: true,
+          msg: 'You must enter a question'
         }
+      }
+    },
+    status: {
+      type: Sequelize.ENUM('open', 'closed'),
+      defaultValue: 'open'
     }
-}, {
-        hooks: {
-            beforeCreate: function (poll) {
-                if (!poll.ownerId) {
-                    throw new Error('poll must have ownerId')
-                }
-            }
+  },
+  {
+    hooks: {
+      beforeCreate: function(poll) {
+        if (!poll.ownerId) {
+          throw new Error('poll must have ownerId');
         }
-    });
+      }
+    }
+  }
+);
 
 module.exports = Poll;
